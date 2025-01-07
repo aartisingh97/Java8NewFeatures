90b5483fadab6bb093cc0625ed8d91f9a442fbf3
Aarti <aartisingh972307@gmail.com>
2025-01-07 16:34:47 +0530

    updated existing file

--- Java8Features/src/com/newFeatures/basic/DuplicateNumbers.java
+++ Java8Features/src/com/newFeatures/basic/DuplicateNumbers.java
@@ -2,8 +2,11 @@ package com.newFeatures.basic;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DuplicateNumbers {
@@ -13,6 +16,28 @@ public class DuplicateNumbers {
 		List<Integer> list = Arrays.asList(5, 3, 4, 1, 3, 7, 2, 9, 9, 4);
 		Set<Integer> result = findDuplicateByCollectionsFrequency(list);
 		result.forEach(System.out::println);
+		Set item = new HashSet<>();
+
+		System.out.println("1. Anaother way of finding duplicates"
+				+ list.stream().filter(n -> !item.add(n)).collect(Collectors.toSet()));
+
+		System.out.println("2. Another way of finding duplicates" + list.stream()
+				// Group the elements along
+				// with their frequency in a map
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+
+				// Convert this map into a stream
+				.entrySet().stream()
+
+				// Check if frequency > 1
+				// for duplicate elements
+				.filter(m -> m.getValue() > 1)
+
+				// Find such elements
+				.map(Map.Entry::getKey)
+
+				// And Collect them in a Set
+				.collect(Collectors.toSet()));
 
 	}
 
--- Java8Features/src/com/newFeatures/basic/SecondHighestLowest.java
+++ Java8Features/src/com/newFeatures/basic/SecondHighestLowest.java
@@ -3,16 +3,24 @@ package com.newFeatures.basic;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SecondHighestLowest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> list = Arrays.asList(0, 3, 10, 20, 30, 15, 1, 13, 1, 2, 2, 10, 40, 19, 40);
+		List<Integer> list = Arrays.asList(0, 3, 10, 20, 30, 15, 1, 13, 1, 2, 2, 10, 40, 19, 40, 41);
 		int secondHighest = list.stream().sorted(Collections.reverseOrder()).distinct().skip(1).findFirst().get();
 		int secondLowest = list.stream().sorted().distinct().skip(1).findFirst().get();
 		System.out.println("Second Highest element: " + secondHighest);
 		System.out.println("Second lowest element: " + secondLowest);
+		int highest = list.stream().sorted(Collections.reverseOrder()).distinct().findFirst().get();
+		System.out.println("Highest element: " + highest);
+
+		System.out.println("Lowest element: " + list.stream().sorted().distinct().findFirst().get());
+
+		System.out.println("Duplicate emements:"
+				+ list.stream().filter(i -> Collections.frequency(list, i) > 1).collect(Collectors.toSet()));
 
 	}
 
