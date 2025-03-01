0c9a9f60e164dba654bcbd7366ceff7c26782857
Aarti <aartisingh972307@gmail.com>
2025-03-01 22:09:31 +0530

    Thread

---
+++ Java8Features/src/com/Thread/practice/ThreadBasic.java
@@ -0,0 +1,22 @@
+package com.Thread.practice;
+
+public class ThreadBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread t = new MyThread();
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main thread");
+		}
+	}
+
+}
+
+class MyThread extends Thread {
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child thread");
+		}
+	}
+}
