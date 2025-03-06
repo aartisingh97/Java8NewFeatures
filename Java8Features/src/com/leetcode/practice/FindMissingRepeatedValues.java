eb112a25bbacaa963300fc034608e27a3d20d158
Aarti <aartisingh972307@gmail.com>
2025-03-06 23:10:49 +0530

    Find missing and the repeated VAlues

---
+++ Java8Features/src/com/leetcode/practice/FindMissingRepeatedValues.java
@@ -0,0 +1,35 @@
+package com.leetcode.practice;
+
+import java.util.Arrays;
+
+public class FindMissingRepeatedValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] g = { { 1, 3 }, { 2, 2 } };
+		FindMissing missing = new FindMissing();
+		System.out.println(Arrays.toString(missing.findMissingAndRepeatedValues(g)));
+	}
+
+}
+
+class FindMissing {
+	public static int[] findMissingAndRepeatedValues(int[][] grid) {
+		int n = grid.length;
+		int[] cnt = new int[n * n + 1];
+		int[] ans = new int[2];
+		for (int[] row : grid) {
+			for (int x : row) {
+				if (++cnt[x] == 2) {
+					ans[0] = x;
+				}
+			}
+		}
+		for (int x = 1;; ++x) {
+			if (cnt[x] == 0) {
+				ans[1] = x;
+				return ans;
+			}
+		}
+	}
+}
